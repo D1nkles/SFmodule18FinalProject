3790d7a61d903b99dfc919530450419f7b0deaba
Ilia <isinetov@mail.ru>
2024-05-28 19:32:47 +0400

    YoutubeClient.Videos.GetAsync() не рабоатет.

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,7 +1,28 @@
-﻿class Program 
+﻿using YoutubeExplode;
+
+class Program 
 {
     static void Main(string[] args) 
     {
+        var a = new VideoManager("https://www.youtube.com/watch?v=kEseOTgX7n4");
+        test();
+        //a.GetVideoInfo();
 
+
+    }
+
+    static async void test() 
+    {
+        var youtube = new YoutubeClient();
+
+        // You can specify either the video URL or its ID
+        var videoUrl = "https://youtube.com/watch?v=u_yIGGhubZs";
+        var video = await youtube.Videos.GetAsync(videoUrl);
+
+        string title = video.Title; // "Collections - Blender 2.80 Fundamentals"
+        string author = video.Author.Title; // "Blender"
+        string duration = video.Duration.ToString(); // 00:07:20
+
+        Console.WriteLine(title + " " + author + " " + duration);
     }
 }
---
+++ Task2/VideoManager.cs
@@ -0,0 +1,27 @@
+﻿using YoutubeExplode;
+using YoutubeExplode.Converter;
+
+class VideoManager
+{
+    YoutubeClient youtubeClient;
+    string _videoURL;
+
+    public VideoManager(string videoURL)
+    {
+        _videoURL = videoURL;
+        youtubeClient = new YoutubeClient();
+    }
+
+    public async void GetVideoInfo() 
+    {
+        var videoDescription = await youtubeClient.Videos.GetAsync(_videoURL);
+        string title = videoDescription.Title;
+        Console.WriteLine(title);
+    }
+
+    public void DownloadVideo() 
+    {
+        youtubeClient.Videos.DownloadAsync(_videoURL, Environment.CurrentDirectory, builder => builder.SetPreset(ConversionPreset.UltraFast));
+    }
+}
+
